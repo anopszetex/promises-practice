83a79f62e5fa171b1109cb68980779cdf0aff165
Andre Luis <andre.1357.luis@gmail.com>
2022-06-02 22:11:27 -0300

    ✅ add method catch and reject

--- tests/unit/newPromise.test.js
+++ tests/unit/newPromise.test.js
@@ -23,7 +23,7 @@ describe('Promise', () => {
 
     test('should call then just when the async code is resolved', async () => {
       const promise = new NPromise(resolve => {
-        setTimeout(() => resolve({ data: 'fake'}), 1e2)
+        setTimeout(() => resolve({ data: 'fake'}), 10)
       })
 
       await expect(promise)
@@ -42,5 +42,72 @@ describe('Promise', () => {
       .toEqual({ data: 'fake' })
     })
 
+
+    test('should support chain of promises on which promises are returned', async () => {
+      const fakeFSPromise = new Promise(resolve => {
+         setTimeout(() => {
+          resolve({ file: 'photo.jpg' }, 10)
+        })
+      })
+      
+
+      new NPromise(resolve => {
+        setTimeout(() => resolve({data: 'promise1'}), 1)
+      }).then(response => {
+        expect(response.data).toBe('promise1')
+
+        return fakeFSPromise
+      })
+      .then(response => {
+        expect(response.file).toBe('photo.jpg')
+      })
+
+    });
+
   })
+
+  describe('Error handling', () => {
+    test('should call catch when an error is throw', async () => {
+      const errorMessage = 'Promise has been rejected'
+
+      return new NPromise((resolve, reject) => {
+        setTimeout(() => reject(new Error(errorMessage)), 10)
+      })
+      .catch(error => {
+        expect(error.message).toBe(errorMessage)
+      })
+    })
+
+    test('should allow catch to be thenable', () => {
+      const errorMessage = 'Promise has been rejected'
+
+      return new NPromise((resolve, reject) => {
+        setTimeout(() => reject(new Error(errorMessage)), 10)
+      })
+      .catch(error => {
+        expect(error.message).toBe(errorMessage)
+        return { data: 'someData' }
+      })
+      .then((response) => {
+        expect(response.data).toBe('someData')
+      })
+    })
+
+    test('should allow to catch an error thrown by a previous catch method', () => {
+      const errorMessage = 'Promise has been rejected'
+
+      return new NPromise((resolve, reject) => {
+        setTimeout(() => reject(new Error(errorMessage)), 10)
+      })
+      .catch(error => {
+        expect(error.message).toBe(errorMessage)
+       throw new Error('That is another error')
+      })
+      .catch((error) => {
+        expect(error.message).toBe('That is another error')
+      })
+    })
+
+  })
+
 })
