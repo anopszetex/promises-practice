d331efdfbf332f7f0863b5b1601e8c2da96dbbc2
Andre Luis <andre.1357.luis@gmail.com>
2022-06-02 22:11:14 -0300

    ✨ add method catch and reject

--- src/newPromise.js
+++ src/newPromise.js
@@ -15,7 +15,7 @@ class NPromise {
     this.onFulFillChain = []
     this.onRejectCallChain = []
 
-    executor(this.resolve.bind(this))
+    executor(this.resolve.bind(this), this.reject.bind(this))
   }
 
   then (onFulFill) {
@@ -33,9 +33,32 @@ class NPromise {
     })
   }
 
+  catch (onReject) {
+    return new NPromise((resolve, reject) => {
+      const onRejected = res => {
+        try {
+          resolve(onReject(res))
+        } catch (error) {
+          reject(error)
+        }
+      }
+
+      if (this.state !== STATE.REJECTED) {
+        this.onRejectCallChain.push(onRejected)
+        return
+      }
+
+      onRejected(this.value)
+    })
+  }
+
   resolve (res) {
     if (this.state !== STATE.PENDING) {
       return
+    }
+
+    if (res !== null && typeof (res?.then) === 'function') {
+      return res.then(this.resolve.bind(this))
     }
 
     this.state = STATE.FULFILLED
@@ -43,6 +66,19 @@ class NPromise {
 
     for (const onFullfilled of this.onFulFillChain) {
       onFullfilled(this.value)
+    }
+  }
+
+  reject (error) {
+    if (this.state !== STATE.PENDING) {
+      return
+    }
+
+    this.state = STATE.REJECTED
+    this.value = error
+
+    for (const onRejected of this.onRejectCallChain) {
+      onRejected(error)
     }
   }
 }
